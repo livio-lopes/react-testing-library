87e3212a12e3ceaa2b835a2ba7995f226666a154
Lívio Lopes <antonio.lvio760@gmail.com>
2023-02-03 13:19:54 -0300

    Requisito 4 resolvido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen, act } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste pagina NotFound', () => {
+  it('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => { history.push('/bode'); });
+    const titleNotFound = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => { history.push('/bode'); });
+    const gifNotFound = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(gifNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
